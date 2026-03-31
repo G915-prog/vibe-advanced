b75a2f2e8133811234635e747760d13421579410
J <gandalph915@proton.me>
2026-03-31 16:38:32 +0200

    routing module 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Module1 from './pages/Module1'
+import Module2 from './pages/Module2';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/module/1" element={<Module1 />} />
+        <Route path="/module/2" element={<Module2 />} />
       </Routes>
     </BrowserRouter>
   )
