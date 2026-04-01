994c85b218089b9a14c91649441e76251e3a5de0
J <gandalph915@proton.me>
2026-04-01 17:54:43 +0200

    routing module4

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Login from './pages/Login'
 import Module1 from './pages/Module1'
 import Module2 from './pages/Module2'
 import Module3 from './pages/Module3'
+import Module4 from './pages/Module4'
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
           <Route path="/module/1" element={<ProtectedRoute><Module1 /></ProtectedRoute>} />
           <Route path="/module/2" element={<ProtectedRoute><Module2 /></ProtectedRoute>} />
           <Route path="/module/3" element={<ProtectedRoute><Module3 /></ProtectedRoute>} />
+          <Route path="/module/4" element={<ProtectedRoute><Module4 /></ProtectedRoute>} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
