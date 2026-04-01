8ef0e1afd0e568976e830a6b308833bb306057ae
J <gandalph915@proton.me>
2026-04-01 16:54:54 +0200

    unfucking

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -8,5 +8,4 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-export const supabase = createClient(url, key)
 window.supabase = supabase  // add this temporarily
